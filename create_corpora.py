432adc4383fdddcc4003ed0c3c917c8ae991723f
AnnaMariaAuer <annamaria.auer0706@gmail.com>
2019-03-05 15:08:47 +0100

    saved corpora without stopword --> question (remove words lower than specific number of characters?)

--- create_corpora.py
+++ create_corpora.py
@@ -2,9 +2,17 @@ import glob
 import re
 import os
 import string
+import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+STOPWORDS = stopwords.words('german')
+print(STOPWORDS)
+# from nltk.tokenize import word_tokenize
 
 
-#Reading in files from a certain path and create one txt-file out of it: all files of one category
+
+# Reading in files from a certain path and create one txt-file out of it: all files of one category
 
 def make_txt_to_category(path, name, save_to_folder):
 
@@ -19,7 +27,6 @@ def make_txt_to_category(path, name, save_to_folder):
     output_file = open(complete_name, 'a+', encoding='UTF-8')
     for file in all_files:
         f = open(file, 'r')
-        #sys.stdout.write(f.read())
         output_file.write(f.read())
         f.close()
     output_file.close()
@@ -81,9 +88,51 @@ def remove_punctuation_numbers (path, save_to_folder):
         counter += 1
 
 
+def make_corpus_without_stopwords(path, save_to_directory):
+    # todo: ich finde wir sollten alle "Wörter" entfernen, die kleiner gleich 3 Buchstaben haben, zumindest alle einzelnen Buchstaben!
+    files_to_process = path + "/*.txt"
+    files = glob.glob(files_to_process)
+    print(files)
+
+    filenames = []
+    translator2 = str.maketrans('', '', string.punctuation)
+
+    counter = 0
+
+    for file in files:
+
+        #gets the names and saves them to list
+        filename = os.path.splitext(file)
+        filenames.append(re.findall(r'[ \w]*all', str(filename[0])))
+
+
+        #removes punctuation, numbers, strip whitespace, to lower
+        current_name = str(filenames[counter]).translate(translator2)
+        print(current_name)
+        complete_name = os.path.join(save_to_directory, current_name + '.txt')
+
+        current_file = open(complete_name, 'a+', encoding='UTF-8')
+        f = open(file, 'r', encoding='UTF-8')
+        words_of_file = f.read().split()
+        no_stopwords = []
+        for word in words_of_file:
+            print(word)
+            if word not in STOPWORDS:
+                no_stopwords.append(word)
+
+        text_for_output = ' '.join(no_stopwords)
+        current_file.write(text_for_output)
+
+        f.close()
+        current_file.close()
+
+        counter += 1
+    print(filenames)
+
+
 
 if __name__ == "__main__":
     # make_txt_to_category("/Users/Anna/PycharmProjects/Stylometry-DH/Arbeitskorpus_subject_matter/Social security for migrant workers", "social_security_for_migrant_workers", "/Users/Anna/PycharmProjects/Stylometry-DH/Arbeitskorpus_subject_matter")
     # get_all_txt_of_category("/Users/Anna/PycharmProjects/Stylometry-DH/Arbeitskorpus_subject_matter", "subject_matter")
-    remove_punctuation_numbers(r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\Arbeitskorpus_advocate_general_structured", r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\corpus-without-punct-numb\advocats")
-
+    # remove_punctuation_numbers(r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\Arbeitskorpus_advocate_general_structured", r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\corpus-without-punct-numb\advocats")
+    make_corpus_without_stopwords(r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\corpus-without-punct-numb\subject-matter", r"C:\Users\ArbeitsPC\PycharmProjects\Stylometry-DH\corpus-without-stopword\subject-matter")
--- testing.py
+++ testing.py
@@ -1,17 +1,32 @@
 import string
 import re
+from nltk.corpus import stopwords
+STOPWORDS = stopwords.words('german')
+print(STOPWORDS)
 
 translator = str.maketrans('','', string.digits)
-s="'12 Juni 2018  1  „Vorlage zur Vorabentscheidung – Art 49 AEUV – Körperschaftsteuer – Niederlassungsfreiheit – Gebietsansässige Gesellschaft – Steuerpflichtiger Gewinn – Steuerliche Entlastung – Abzug der Verluste gebietsansässiger Betriebsstätten – Bewilligung – Abzug der Verluste gebietsfremder Betriebsstätten – Ausschluss – Ausnahme – Fakultative Regelung der internationalen gemeinsamen Besteuerung“" \
+s="' aber 12 Juni 2018 alle 1  „Vorlage zur Vorabentscheidung – Art 49 AEUV – Körperschaftsteuer – Niederlassungsfreiheit – Gebietsansässige Gesellschaft – Steuerpflichtiger Gewinn – Steuerliche Entlastung – Abzug der Verluste gebietsansässiger Betriebsstätten – Bewilligung – Abzug der Verluste gebietsfremder Betriebsstätten – Ausschluss – Ausnahme – Fakultative Regelung der internationalen gemeinsamen Besteuerung“" \
   "In der Rechtssache C‑65016 " \
   "betreffend ein Vorabentscheidungsersuchen nach Art 267 AEUV eingereicht vom Østre Landsret Landgericht der Region Ost Dänemark mit Entscheidung vom 12 Dezember 2016 beim Gerichtshof eingegangen am 19 Dezember 2016 in dem Verfahren" \
   "AS Bevola" \
-  "Jens W Trock ApS'"
+  "Jens W Trock ApS' und"
 
-print(re.sub(r'[^\w\s]','',s))
+# print(re.sub(r'[^\w\s]','',s))
 
-print(s.translate(translator).lower())
+# print(s.translate(translator).lower())
 
 no_numbers_lower = s.translate(translator).lower()
 no_whitespace = " ".join(no_numbers_lower.split())
 print(no_whitespace)
+
+no_stopwords = []
+words = no_whitespace.split()
+
+for word in words:
+  print(word)
+  if word not in STOPWORDS:
+    no_stopwords.append(word)
+
+text_for_output = ' '.join(no_stopwords)
+print(no_stopwords)
+print(text_for_output)
